5717d745bf3e73246146ef715e128f7aa2c16cdd
Elena <15el.shev@gmail.com>
2022-07-12 21:26:37 +0300

    Add adaptiv form

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./page/Login";
 import SingUp from "./page/SingUp";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
